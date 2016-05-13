4bbe0a764a5951ad6bc368066ab601226915c16e
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-13 11:04:29 -0400

    Added method :GetUSerName

--- COMP123_S2016_Lesson1B/Program.cs
+++ COMP123_S2016_Lesson1B/Program.cs
@@ -4,14 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Aslan Mirsakiyev
+// #300850326
+// 13 of May 2016
+// Version 1.0
 namespace COMP123_S2016_Lesson1B
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // Aslan Mirsakiyev
-            // 300850326
+            GetUserName();
+        }
+
+        /**
+        * This method reads the UserName from the Console
+        * @method GetUserName
+        * @return {string} UserName
+        */
+
+        public static string GetUserName()
+        {
+            // Initilize Variables
+            
+            string UserName = "";
+            Console.Write("Enter User Name: ");
+            UserName=Console.ReadLine();
+
+            Console.WriteLine("/n_________________________");
+            Console.WriteLine("You entered: " + UserName);
+
+            return UserName;
         }
     }
 }
