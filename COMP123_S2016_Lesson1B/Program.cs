3c30bfe677d842645ef6954eda9212068eeb3dd9
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-13 11:12:40 -0400

    Updated comments for my driver class

--- COMP123_S2016_Lesson1B/Program.cs
+++ COMP123_S2016_Lesson1B/Program.cs
@@ -5,14 +5,27 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-// Aslan Mirsakiyev
-// #300850326
-// 13 of May 2016
-// Version 1.0
+/**
+* Aslan Mirsakiyev
+* #300850326
+* 13 of May 2016
+* Version 0.3-Updated comments for my driver class
+*/
 namespace COMP123_S2016_Lesson1B
 {
+    /**
+    * This is driver class for our program
+    *
+    * @class Program
+    */
     class Program
     {
+        /**
+        * Main method for class Program
+        * 
+        * @param {string[]} args
+        * @constructor Main
+        */
         static void Main(string[] args)
         {
             GetUserName();
